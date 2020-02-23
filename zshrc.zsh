77ca3441c1c5c855c31a96a972a5fd62f89028b3
Andrew <deathangel908@gmail.com>
2020-02-23 12:12:06 +0200

    Update zshrc.zsh

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,6 +1,3 @@
-
-# My bash
-
 txtblk='\e[0;30m' # Black - Regular
 txtred='\e[0;31m' # Red
 txtgrn='\e[0;32m' # Green
@@ -50,24 +47,23 @@ precmd() {
 	echo -ne "$txtrst:"
   if 	[[ $(whoami) == root ]] ; then
 	 echo -ne "$txtred"
-	else 
+	else
    echo -ne "$txtgrn"
 	fi
 	echo -ne "`whoami`$txtrst:$txtpur`pwd`$txtrst:"
-  if [[ $exit_code != 0 ]] ; then 
+  if [[ $exit_code != 0 ]] ; then
    echo -ne "$bldred"
-	else 
+	else
 	 echo -ne "$bldgrn"
   fi
 	echo "$exit_code"
 }
 
 
-HISTFILE=/Users/deathangel908/.zhistory
 HISTSIZE=10000
 SAVEHIST=10000
-disable log
 setopt incappendhistory
+unsetopt BEEP
 
 set -o vi
 
@@ -89,21 +85,39 @@ export CPPFLAGS="-I/usr/local/opt/krb5/include"
 export NVM_DIR="$HOME/.nvm"
 
 alias mc="PAGER=open /usr/local/bin/mc"
+alias sshs="ssh root@pychat.org -p 8886"
+
 
 reset_idea() {
-
   reset_idea_ide ()  {
      echo "Removing $1 $2"
      prefs_dir=/Users/deathangel908/Library/Preferences
      ide_dir=$prefs_dir/$1
+
      rm -rfv $ide_dir/eval
      sed -i '' '/evlsprt/d' $ide_dir/options/other.xml;
      rm -v $prefs_dir/jetbrains.$2.*.plist
   }
-
   reset_idea_ide Pycharm2019.3 pycharm
   reset_idea_ide WebStorm2019.3 webstorm
   reset_idea_ide DataGrip2019.3 datagrip
   unset -f reset_idea_ide
 }
-source ~/.zshrc.local
+
+sshgen() {
+        if [ -z "$1" ]; then
+                echo "Generates rsa key on server, usage:"
+                echo "sshgen andrew@192.168.1.100"
+		echo "Or if port is required:"
+		echo "sshgen andrew@192.168.1.100 -p 123"
+        else
+                if [ ! -f ~/.ssh/id_rsa.pub ]; then
+                        echo "Generating rsa"
+                        mkdir -p ~/.ssh
+                        ssh-keygen -t rsa
+                fi
+                cat ~/.ssh/id_rsa.pub | ssh $@ "cat > /tmp/r2d2_auth_keys && mkdir -p ~/.ssh && cat /tmp/r2d2_auth_keys >> ~/.ssh/authorized_keys"
+        fi
+}
+
+source /Users/deathangel908/.zshrc.local
